ced4f1c8092ceec635adf54b1e8d7f0a688a466a
obarra <barra_omar@hotmail.com>
2019-08-17 23:13:27 -0300

    add dynamic port

--- item-service/src/main/java/com/omm/item/controller/ItemController.java
+++ item-service/src/main/java/com/omm/item/controller/ItemController.java
@@ -15,7 +15,7 @@ import com.omm.item.service.ItemService;
 public class ItemController {
 
 	@Autowired
-	@Qualifier("itemServiceRestTemplate")
+	@Qualifier("itemServiceFeign")
 	private ItemService itemService;
 	
 	@GetMapping("/list")
