bcd66a2196b4aec33ee9854df42fc7fa0e27f74f
obarra <barra_omar@hotmail.com>
2019-08-17 21:54:54 -0300

    configure client eureka

--- item-service/src/main/java/com/omm/item/ItemServiceApplication.java
+++ item-service/src/main/java/com/omm/item/ItemServiceApplication.java
@@ -2,9 +2,11 @@ package com.omm.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @RibbonClient(name = "product-service")
 @EnableFeignClients
 @SpringBootApplication
