6137360e18ced3aca97ffb621dc419e7b210e629
obarra <barra_omar@hotmail.com>
2020-02-15 16:22:50 -0300

    integration test

--- spring-boot-webflux/src/test/java/com/obarra/webflux/WebfluxApplicationTests.java
+++ spring-boot-webflux/src/test/java/com/obarra/webflux/WebfluxApplicationTests.java
@@ -1,13 +1,29 @@
 package com.obarra.webflux;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+//  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebfluxApplicationTests {
+
+	// already configure and ready to issue requests against "localhost:RANDOM_PORT
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
+		webTestClient.get().uri("/cryptocurrency-handler/life")
+				.accept(MediaType.TEXT_PLAIN)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("It's alive");
 	}
 
 }
