bab0d0ff1288a898e1d66deb651f0e4b9d7d4999
obarra <barra_omar@hotmail.com>
2020-02-27 17:56:23 -0300

    other configs for rest repository

---
+++ user-service/src/main/java/com/obarra/user/config/RepositoryConfig.java
@@ -0,0 +1,16 @@
+package com.obarra.user.config;
+
+import com.obarra.user.entity.Role;
+import com.obarra.user.entity.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(User.class, Role.class);
+    }
+}
--- user-service/src/main/java/com/obarra/user/repository/UserRepository.java
+++ user-service/src/main/java/com/obarra/user/repository/UserRepository.java
@@ -5,13 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     User findByUsername(String username);
 
+    @RestResource(path = "find-by-email")
     @Query("select u from User u where u.email=:email")
-    User findUser(@Param("email") String email);
+    User findMatchEmail(@Param("email") String email);
 
 }
