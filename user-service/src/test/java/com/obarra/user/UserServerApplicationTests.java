9e17af193721cbed75e7fa54c6bbb1a69209bb5b
obarra <barra_omar@hotmail.com>
2020-02-27 20:27:02 -0300

    change to spring boot 2.1.7 because superior has conflict with rabbit mq

--- user-service/src/test/java/com/obarra/user/UserServerApplicationTests.java
+++ user-service/src/test/java/com/obarra/user/UserServerApplicationTests.java
@@ -1,12 +1,11 @@
 package com.obarra.user;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServerApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
