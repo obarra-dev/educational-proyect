18f2b22a745ec700bcd110b58ec7862d48f8032f
obarra <barra_omar@hotmail.com>
2020-02-27 20:35:06 -0300

    oauth2 with eureka, config, zipking, zuul

---
+++ oauth2-service/src/main/java/com/obarra/oauth2/Oauth2ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.obarra.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ServiceApplication.class, args);
+	}
+
+}
---
+++ oauth2-service/src/test/java/com/obarra/oauth2/Oauth2ServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.obarra.oauth2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ServiceApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
